340707625195e3701c8e52fcfcfa737505a9cf75
Alexandre Dantas dos Santos <allexkid@info.ufrn.br>
2025-01-01 22:44:07 -0300

    Criando projeto Web.

---
+++ estoquepreco/src/main/java/com/microservico/estoquepreco/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.microservico.estoquepreco;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EstoqueprecoApplication.class);
+	}
+
+}
